1161f69bb521672bb08b65422daae74fefcf3072
mdasik0 <asikthe1st@gmail.com>
2023-08-13 21:57:09 +0600

    git layout done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-import Link from "next/link";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/Shared/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
         <main className="max-w-[1380px] mx-auto">{children}</main>
       </body>
     </html>
